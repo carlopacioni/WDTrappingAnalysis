566ed2bd81c6c22491e701f256e2ee06c17fbab2
Carlo Pacioni <carlo.pacioni@gmail.com>
2020-08-06 17:02:20 +1000

    renamed prelim script

--- Trapping_Analysis_scats_preliminary.R
+++ Trapping_Analysis_scats_preliminary.R
@@ -67,7 +67,7 @@ nt<- 10
 nc<- 5
 np <- 8 # Number of CPUs
 
-fitBern_indb0 = jags(dat, inits, params, model.file="./Models/SimpleBernModel_indb0.txt", 
+fitBern_indb0 = jags(dat, inits, params, model.file="./Models/BernModel_indb0.txt", 
               n.chains=nc, n.iter=ni, n.burnin=nb, 
               n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
               n.cores=ifelse(floor(nc/np) < np, nc, np))
@@ -83,42 +83,17 @@ ggplot(data.frame(mu.b0=fitBern_indb0$sims.list$mu.b0,
                   Iter=seq_along(fitBern_indb0$sims.list$mu.b0))) + 
   geom_line(aes(Iter, mu.b0))
 
-#### fit with covariates (ndays and distance) TO-DO #### 
-dat <- list(y=caphist_m, nind=length(IDs), nobs=nobs, d=dist, t=time_diff)
-# arguments 
-params = c("b0", "bd", "bt")
 
-inits <- function() {
-  list(b0=rnorm(1, -3, 0.5),
-       bd=rnorm(1, 0, 0.5),
-       bt=rnorm(1, 0, 0.5))
-}
-
-# call to JAGS
-ni<- 7000
-nb<- 5000
-nt<- 1
-nc<- 3
-np <- 8 # Number of CPUs
-
-fitCov = jags(dat, inits, params, model.file="./Models/BernModel_time_dist.txt", 
-           n.chains=nc, n.iter=ni, n.burnin=nb, 
-           n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
-           n.cores=ifelse(floor(nc/np) < np, nc, np))
-
-print(fitCov, digits=3)
-sapply(fitCov$n.eff, summary)
-fitCov$Rhat
-plot(fitCov)
-
-#### fit with distance TO-DO ####
+#### fit with distance ####
 dat <- list(y=caphist_m, nind=length(IDs), nobs=nobs, d=dist)
 # arguments 
-params = c("b0", "bd")
+params = c("mu.b0", "sigma.b0", "mu.bd", "sigma.bd")
 
 inits <- function() {
-  list(b0=rnorm(1, -3, 0.5),
-       bd=rnorm(1, -1, 0.5)
+  list(mu.b0=rnorm(1, -3, 0.5),
+       sigma.b0=runif(1, 0.2, 0.75),
+       mu.bd=rnorm(1, -3, 0.5),
+       sigma.bd=runif(1, 0.2, 0.75)
        )
 }
 
@@ -129,86 +104,23 @@ nt<- 1
 nc<- 3
 np <- 8 # Number of CPUs
 
-fitDist = jags(dat, inits, params, model.file="./Models/BernModel_dist.txt", 
+fitBernDist_ind = jags(dat, inits, params, model.file="./Models/BernModel_dist_ind.txt", 
               n.chains=nc, n.iter=ni, n.burnin=nb, 
               n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
               n.cores=ifelse(floor(nc/np) < np, nc, np))
 
-print(fitDist, digits=3)
-sapply(fitDist$n.eff, summary)
-fitDist$Rhat
-plot(fitDist)
+print(fitBernDist_ind, digits=3)
+sapply(fitBernDist_ind$n.eff, summary)
+fitBernDist_ind$Rhat
+plot(fitBernDist_ind)
+inv.logit(fitBernDist_ind$mean$mu.b0 + fitBernDist_ind$mean$mu.bd * mean(dist))
+1-(1-inv.logit(fitBernDist_ind$mean$mu.b0 + fitBernDist_ind$mean$mu.bd * mean(dist)))^(ndays*ntraps) # Prob for ntraps deployed ndays
 
 #### Survivival ####
 
-#### fit simple survival ####
-dat <- list(y=surv_df$censored, nind=length(IDs), t=surv_df$t.comb, 
-            nobs=nrow(surv_df), ind=surv_df$ind)
-# arguments 
-params = c("v", "b0", "mu.b0", "sigma.b0")
-
-inits <- function() {
-  list(mu.b0=rnorm(1, 0, 0.5),
-       sigma.b0=runif(1, 0.2, 0.75),
-       v=rgamma(1, 2.5, 3))
-}
-
-# call to JAGS
-ni<- 50000
-nb<- 4000
-nt<- 10
-nc<- 4
-np <- 8 # Number of CPUs
-
-fitSurv_indb0 = jags(dat, inits, params, model.file="./Models/SurvModel_indb0.txt", 
-           n.chains=nc, n.iter=ni, n.burnin=nb, 
-           n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
-           n.cores=ifelse(floor(nc/np) < np, nc, np))
-
-print(fitSurv_indb0, digits=3)
-sapply(fitSurv_indb0$n.eff, summary)
-plot(fitSurv_indb0, parameters=c("v", "mu.b0", "sigma.b0"))
-
-save(fitSurv_indb0, file=file.path(fittedMods, "fitSurv_indb0.rda"))
-load(file=file.path(fittedMods, "fitSurv_indb0.rda"))
 
 
-# S_t = exp(-lambda*t^v)
-S_t <- exp(-exp(fitSurv_indb0$mean$mu.b0) * (seq_len(ndays)/time_period)^fitSurv_indb0$mean$v)
-p <- inv.cloglog(fitSurv_indb0$mean$mu.b0 + log((seq_len(ndays)/time_period)) * fitSurv_indb0$mean$v)
-df <- data.frame(p, days=seq_len(ndays), p_nTraps=1-(1-p)^ntraps)
-ggplot(df) + geom_line(aes(days, p)) + geom_line(aes(days, p_nTraps), col="red")
-
-#### fit survival with distance TO-DO ####
-dat <- list(y=caphist_m, nind=length(IDs), nobs=nobs, t=time_diff, d=dist)
-# arguments 
-params = c("v", "b0", "bd")
-
-inits <- function() {
-  list(b0=rnorm(1, 0, 0.5),
-       v=rgamma(1, 2.5, 3))
-}
-
-# call to JAGS
-ni<- 200
-nb<- 100
-nt<- 1
-nc<- 1
-np <- 8 # Number of CPUs
-
-fitSurvDist_indb0 = jags(dat, inits, params, model.file="./Models/SurvModel_dist.txt", 
-               n.chains=nc, n.iter=ni, n.burnin=nb, 
-               n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
-               n.cores=ifelse(floor(nc/np) < np, nc, np))
-
-print(fitSurvDist_indb0, digits=3)
-sapply(fitSurvDist_indb0$n.eff, summary)
-fitSurvDist_indb0$Rhat
-plot(fitSurvDist_indb0)
-save(fitSurvDist_indb0, file = file.path(fittedMods, "fitSurvDist_indb0.rda"))
-load(file.path(fittedMods, "fitSurvDist_indb0.rda"))
-
-#### fit survival without distance Weibull ####
+#### fit survival Weibull Individual traps ####
 dat=list(censored=surv_df$censored, t=surv_df$TimeTrap, 
          t.cen=surv_df$t.cen, nobs=nrow(surv_df), ind=surv_df$ind, nind=length(IDs))
 
@@ -246,10 +158,40 @@ p_Weib <- inv.cloglog(fitSurvWeib_indb0$mean$mu.b0 + log((seq_len(ndays)/time_pe
 df_Weib <- data.frame(p=p_Weib, days=seq_len(ndays), p_nTraps=1-(1-p_Weib)^ntraps)
 ggplot(df_Weib) + geom_line(aes(days, p)) + geom_line(aes(days, p_nTraps), col="red")
 
-#### fit survival with distance Weibull TO-DO ####
+#### fit survival without distance Weibull ####
+dat=list(censored=surv_df$censored, t=surv_df$TimeTrap, 
+         t.cen=surv_df$t.cen, nobs=nrow(surv_df))
 
-dat=list(censored=1 - surv_df$NonCensored, t=surv_df$TimeTrap, 
-         t.cen=surv_df$TimeTrap, nobs=nrow(surv_df), d=dist_vect)
+# arguments 
+params = c("v", "b0")
+
+inits <- function() {
+  list(b0=rnorm(1, 0, 0.5),
+       v=rgamma(1, 2.5, 3),
+       t=surv_df$t.start)
+}
+
+# call to JAGS
+ni<- 50000
+nb<- 4000
+nt<- 1
+nc<- 2
+np <- 8 # Number of CPUs
+
+fitSurvWeib = jags(dat, inits, params, model.file="./Models/SurvModel_WeibT.txt", 
+                   n.chains=nc, n.iter=ni, n.burnin=nb, 
+                   n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
+                   n.cores=ifelse(floor(nc/np) < np, nc, np))
+
+print(fitSurvWeib, digits=3)
+plot(fitSurvWeib)
+save(fitSurvWeib, file="fitSurvWeib.rda")
+load(file="fitSurvWeib.rda")
+
+#### fit survival with distance Weibull ####
+
+dat=list(censored=1 - surv_df$censored, t=surv_df$TimeTrap, 
+         t.cen=surv_df$t.cen, nobs=nrow(surv_df), d=dist_vect)
 
 # arguments 
 params = c("v", "b0", "bd")
@@ -318,6 +260,47 @@ p_WeibTrapEffort <- plot_probs(lam=exp(fitSurvWeibTrapEff$mean$b0), max.ndays=28
 p_WeibTrapEffort
 
 ggsave(file.path(results, "plot_WeibTrapEffort.pdf"), plot = p_ExpTrapEffort)
+
+OneDayOnetrap <- 1/(time_period * ntraps)
+pOneTrapOneDay <- 1 - S_t(lam = exp(fitSurvWeibTrapEff$mean$b0), t = OneDayOnetrap, v = fitSurvWeibTrapEff$mean$v)
+pOneTrapOneDay
+
+#### fit survival  Weibull trap effort individual dog coefs ####
+params = c("v", "b0", "mu.b0", "sigma.b0")
+
+inits <- function() {
+  list(mu.b0=rnorm(1, 0.1, 0.5),
+       sigma.b0=runif(1, 0.2, 0.75),
+       v=rgamma(1, 2.5, 3),
+       t=surv_eff$t.start)
+}
+
+# call to JAGS
+ni<- 600000
+nb<- 40000
+nt<- 10
+nc<- 3
+np <- 8 # Number of CPUs
+
+fitSurvWeibTrapEff_indDogCoefs = jags(dat, inits, params, model.file="./Models/SurvModel_WeibTrap_eff_indDogCoef.txt", 
+                          n.chains=nc, n.iter=ni, n.burnin=nb, 
+                          n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
+                          n.cores=ifelse(floor(nc/np) < np, nc, np))
+
+save(fitSurvWeibTrapEff_indDogCoefs, file=file.path(fittedMods, "fitSurvWeibTrapEff.rda"))
+load(file=file.path(fittedMods, "fitSurvWeibTrapEff_indDogCoefs.rda"))
+
+print(fitSurvWeibTrapEff_indDogCoefs, digits=3)
+plot(fitSurvWeibTrapEff_indDogCoefs)
+fitSurvWeibTrapEff_indDogCoefs$mean$v
+fitSurvWeibTrapEff_indDogCoefs$mean$b0
+exp(fitSurvWeibTrapEff_indDogCoefs$mean$b0) # lambda
+#plot(fitSurvWeibTrapEff)
+
+p_fitSurvWeibTrapEff_indDogCoefs <- plot_probs(lam=exp(fitSurvWeibTrapEff_indDogCoefs$mean$b0), max.ndays=28, 
+                               v=fitSurvWeibTrapEff_indDogCoefs$mean$v, 
+                               time.period=time_period, n.traps=ntraps)
+p_fitSurvWeibTrapEff_indDogCoefs
 
 
 #### fit survival  Exp trap effort ####
