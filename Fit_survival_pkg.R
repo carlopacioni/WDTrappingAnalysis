06b56d96f912f41d0d711b25d549777620b24bc6
Carlo Pacioni <carlo.pacioni@gmail.com>
2020-08-09 13:41:27 +1000

    plot with survival package

--- Fit_survival_pkg.R
+++ Fit_survival_pkg.R
@@ -1,5 +1,6 @@
 library(survival)
 expmod <- survreg(Surv(surv_eff$t.comb, 1-surv_eff$censored) ~ 1, data = surv_eff, dist = "exponential")
 summary(expmod)
+plot(survfit(Surv(surv_eff$t.comb, 1-surv_eff$censored)~1))
 1/exp(expmod$coefficients) # lambda == exp(-coeff)
 mean(predict(expmod, type = "response")) # Should be == 1/lambda
