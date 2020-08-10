ef5783b9437a121b653663c98af6fe29ea858134
Carlo Pacioni <carlo.pacioni@gmail.com>
2020-08-11 09:10:15 +1000

    Comb data for cam traps

--- Trapping_Analysis_camTraps.R
+++ Trapping_Analysis_camTraps.R
@@ -72,7 +72,12 @@ p_WeibTrapEffortCamTrapMfd <- plot_probs(lam=exp(fitSurvWeibTrapEffCamTrapMfd$me
                                       max.ndays=28, 
                                v=fitSurvWeibTrapEffCamTrapMfd$mean$v, 
                                time.period=time_period, n.traps=ntraps)
+p_WeibTrapEffortCamTrapMfdHPD <- plot_probsHPD(fittedMod = fitSurvWeibTrapEffCamTrapMfd, 
+                                         max.ndays=28, 
+                                         v="mod", 
+                                         time.period=time_period, n.traps=ntraps)
 p_WeibTrapEffortCamTrapMfd
+p_WeibTrapEffortCamTrapMfdHPD
 
 ggsave(file.path(results, "plot_WeibTrapEffortCamTrapMfd.pdf"), 
        plot=p_WeibTrapEffortCamTrapMfd, width = 18, height = 15, units = "cm")
@@ -216,3 +221,52 @@ ggsave(file.path(results,"plot_ExpTrapEffortCamTrapSck.png"),
        plot = p_ExpTrapEffCamTrapsSck, width = 18, height = 15, units = "cm")
 
 fitSurvExpTrapEffCamTrapsSck$DIC - fitSurvWeibTrapEffCamTrapSck$DIC
+
+
+
+#### Combined data ####
+
+surv_eff_comb_camtraps <- rbind(surv_eff_mfd_camtraps, surv_eff_sck_camtraps)
+surv_eff_comb_camtraps$t.cen <- max(surv_eff_comb_camtraps$t.cen)
+surv_eff_comb_camtraps$t.start <- max(surv_eff_comb_camtraps$t.cen) + 1
+dat=list(censored=surv_eff_comb_camtraps$censored, t=surv_eff_comb_camtraps$TimeTrap, 
+         t.cen=surv_eff_comb_camtraps$t.cen, nind=mfd_nind + sck_nind)
+
+# arguments 
+params = c("b0")
+
+inits <- function() {
+  list(b0=rnorm(1, 0.1, 0.5),
+       t=surv_eff_comb_camtraps$t.start)
+}
+
+fitSurvExpTrapEffCamTrapsCbn = jags(dat, inits, params, model.file="./Models/SurvModel_ExpTrap_eff.txt", 
+                                    n.chains=nc, n.iter=ni, n.burnin=nb, 
+                                    n.thin=nt, parallel=ifelse(nc>1, TRUE, FALSE), 
+                                    n.cores=ifelse(floor(nc/np) < np, nc, np))
+
+print(fitSurvExpTrapEffCamTrapsCbn, digits=3)
+fitSurvExpTrapEffCamTrapsCbn$mean$b0
+exp(fitSurvExpTrapEffCamTrapsCbn$mean$b0) # lambda
+#plot(fitSurvExpTrapEffCamTrapsCbn)
+save(fitSurvExpTrapEffCamTrapsCbn, file=file.path(fittedMods, "fitSurvExpTrapEffCamTrapsCbn.rda"))
+load(file=file.path(fittedMods, "fitSurvExpTrapEffCamTrapsCbn.rda"))
+
+p_ExpTrapEffCamTrapsCbn <- plot_probs(lam=exp(fitSurvExpTrapEffCamTrapsCbn$mean$b0), 
+                                      max.ndays=28, v=1, 
+                                      time.period=time_period, n.traps=ntraps) #+ ylim(c(0, 0.18))
+p_ExpTrapEffCamTrapsCbn 
+
+ggsave(file.path(results,"plot_ExpTrapEffortCamTrapCbn.pdf"), plot = p_ExpTrapEffCamTrapsCbn)
+ggsave(file.path(results,"plot_ExpTrapEffortCamTrapCbn.png"), 
+       plot = p_ExpTrapEffCamTrapsCbn, width = 18, height = 15, units = "cm")
+
+p_WeibTrapEffortCamTrapCbnHPD <- plot_probsHPD(fittedMod = fitSurvExpTrapEffCamTrapsCbn, 
+                                               max.ndays=28, 
+                                               v=1, 
+                                               time.period=time_period, n.traps=ntraps)
+p_WeibTrapEffortCamTrapCbnHPD
+
+ggsave(file.path(results,"plot_ExpTrapEffortCamTrapCbnHPD.pdf"), plot = p_WeibTrapEffortCamTrapCbnHPD)
+ggsave(file.path(results,"plot_ExpTrapEffortCamTrapCbnHPD.png"), 
+       plot = p_WeibTrapEffortCamTrapCbnHPD, width = 18, height = 15, units = "cm")
